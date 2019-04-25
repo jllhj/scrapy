1270972843c8604921b8058ef17bc03393302529
lhj <468867748@qq.com>
2019-04-25 17:49:33 +0800

    爬取知乎作者关注者和粉丝

--- scrapyxdb/pipelines.py
+++ scrapyxdb/pipelines.py
@@ -1,57 +1,26 @@
-# -*- coding: utf-8 -*-
+import pymongo
 
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-"""
-源码内容：
-    1.判断当前XdbPipeline类中是否有from_crawler
-        有：
-            obj = XdbPipeline.from_crawler(.....)
-        否:
-            obj = XdbPipeline()
-    2. obj.open_spider()
-    3. obj.process_item()/process_item()/process_item()
-    4. obj.close_spider()
-"""
+class MongoPipeline(object):
 
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
 
-class ScrapyxdbPipeline(object):
-    def __init__(self,path):
-        self.f = None
-        self.path = path
-
-    @classmethod  # 不需要实例化 直接类名.方法
+    @classmethod
     def from_crawler(cls, crawler):
-        """
-        初始化时候，用于创建pipeline对象
-        :param crawler:
-        :return:
-        """
-        print('File.from_crawler')
-        path = crawler.settings.get('HREF_FILE_PATH')
-        return cls(path)
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE')
+        )
 
     def open_spider(self, spider):
-        """
-        爬虫开始执行时，调用
-        :param spider:
-        :return:
-        """
-        self.f = open(self.path,'a+',encoding='utf-8')
-        print('爬虫开始')
-
-    def process_item(self, item, spider):
-        print('pipelines',item)
-        self.f.write(item['title']+item['up']+item['watch'].strip()+'\n')
-        return item
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
 
     def close_spider(self, spider):
-        """
-        爬虫关闭时，被调用
-        :param spider:
-        :return:
-        """
-        self.f.close()
-        print('爬虫结束')
+        self.client.close()
+
+    def process_item(self, item, spider):
+        # 有去重操作 update 有就更新 没有就插入
+        self.db['user'].update({'url_token':item['url_token']},{'$set':item},True)
+        return item
--- scrapyxdb/settings.py
+++ scrapyxdb/settings.py
@@ -39,10 +39,12 @@ ROBOTSTXT_OBEY = False
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-#}
+DEFAULT_REQUEST_HEADERS = {
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+  'Accept-Language': 'en',
+  'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.53 Safari/537.36'
+}
+
 
 # Enable or disable spider middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
@@ -64,9 +66,9 @@ ROBOTSTXT_OBEY = False
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-# ITEM_PIPELINES = {
-#    'scrapyxdb.pipelines.ScrapyxdbPipeline': 300,
-# }
+ITEM_PIPELINES = {
+   'scrapyxdb.pipelines.MongoPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
@@ -89,3 +91,6 @@ ROBOTSTXT_OBEY = False
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 HREF_FILE_PATH = "news.log"
+
+MONGO_URL = 'localhost'
+MONGO_DATABASE = 'zhihu'
